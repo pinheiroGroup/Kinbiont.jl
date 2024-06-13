9da7cda4d17b06970a3fbaf80f09f9688b01c168
Fabrizio Angaroni <43064628+ang-one@users.noreply.github.com>
2024-06-13 09:17:49 +0200

    Adding data struct for output

--- src/data_struct_Kimchi.jl
+++ src/data_struct_Kimchi.jl
@@ -1,6 +1,7 @@
 struct Kimchi_res_one_well
     method::String
     fit::Vector{Float64}
+    times::Vector{Float64}
     params::Vector{Any}
 end
 
@@ -9,4 +10,29 @@ struct Kimchi_res_one_file
     params::Matrix{Any}
 end
 
-Kimchi_one_file_output = Dict(model.name => model for model in models_list)
+struct Kimchi_res_model_selection
+    method::String
+    fit::Vector{Float64}
+    times::Vector{Float64}
+    params::Vector{Any}
+    AIC::Vector{Float64}
+    selected_model::String
+    model_comparison::Matrix{Any}
+    params_2::Vector{Any}
+end
+
+struct Kimchi_res_sensitivity
+    method::String
+    combinations::Matrix{Any}
+    params::Matrix{Any}
+end
+
+struct Kimchi_res_segmentation_ODE
+    method::String
+    params::Matrix{Any}
+    intervals_cdp::Array{Any}
+    times_of_the_fit::Array{Float64}
+    fit::Array{Float64} 
+    total_loss::Float64
+
+end
