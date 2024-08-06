8c49bfbdf95023facf2381bb0abdbb6b958200b5
Fabrizio Angaroni <43064628+ang-one@users.noreply.github.com>
2024-08-06 08:46:18 +0200

    typos

--- src/ML_downstream.jl
+++ src/ML_downstream.jl
@@ -41,7 +41,7 @@ Function that
 - `n_folds_cv = 3`: Int,   n-fold of the cross validation
 # Output:
 """
-function downstream_decision_tree_regression(kinBiont_results::Matrix{Any}, # output of KinBiont results
+function downstream_decision_tree_regression(KinBiont_results::Matrix{Any}, # output of KinBiont results
   feature_matrix::Matrix{Any},
   row_to_learn::Int;
   max_depth = -1,
@@ -138,117 +138,6 @@ function downstream_decision_tree_regression(kinBiont_results::Matrix{Any}, # ou
     tree_out = DecisionTree.print_tree(model, max_depth)
   end
     return model, imp_1, imp_2, r2 , leaves_values
-end
-
-
-
-
-"""
-    downstream_symbolic_regression(
-    KinBiont_results,
-    feature_matrix,
-    row_to_learn;
-    options = SymbolicRegression.Options(),
-    )
-Function that evalauates 
-# Arguments:
-- `kinBiont_results::Matrix{Any}`: The matrix of results of fitting one file with KinBiont. Compatible functions `fit_file_ODE`,`fit_file_custom_ODE`, `ODE_model_selection_file`, `segmentation_ODE_file`, `fit_NL_model_file`, `fit_NL_model_selection_file`, and `fit_NL_segmentation_file`.
-- `feature_matrix::Matrix{Any}`: Matrix of the features for the ML analysis. Important, the number of rows of this file should be te number of columns (minus one) of the KinBiont_results, and the first column should have a name of the well in order to mach the feature with the names of the well in second row of KinBiont_results. 
-- `row_to_learn::Int`: which row of the matrix `kinBiont_results` will be the target of the ML inference.
-# Key Arguments:
--  'options = SymbolicRegression.Options()' the option class of the symbolic regression class, see example and https://astroautomata.com/SymbolicRegression.jl/stable/api/#SymbolicRegression.CoreModule.OptionsStructModule.Options for details.
-# Outputs:
-if `res =  downstream_symbolic_regression()`:
--`trees`: the trees representing the hall of fames results
--`res_output`: a matrix containing the hall_of_fame  of the inference, where first column is the Complexity score, second column MSE and third column the equation Equation
--`predictions`: For each equation we return the predicted value for each sample (in this matrix equations are columns and rows are the samples)
--`index_annotation`: Index on how to order the rows of features matrix to match the columns of KinBiont results
-"""
-function downstream_symbolic_regression2(kinBiont_results,
-  feature_matrix,
-  row_to_learn;
-  options = SymbolicRegression.Options(),
-  n_processor = 2,
-  niterations = 5
-)
-
-#Distributed.addprocs(n_processor)
-
-
-
-  names_of_the_wells_res = KinBiont_results[2, 1:end]
-  names_of_the_wells_annotation = feature_matrix[1:end, 1]
-  wells_to_use = intersect(names_of_the_wells_res, names_of_the_wells_annotation)
-
-
-  index_res = Int
-  index_annotation = Int
-
-
-
-
-
-  index_res =  findfirst.(isequal.(wells_to_use), (names_of_the_wells_res,))
-  index_annotation =findfirst.(isequal.(wells_to_use), (names_of_the_wells_annotation,))
-
-  index_res = index_res[index_res.!=nothing]
-  index_annotation =index_annotation[index_annotation.!=nothing]
-
-  # order results and annotation by well in the same order
-
-  #index_res[1, :] = index_res[1, :] 
-
-  output = convert.(Float64, KinBiont_results[row_to_learn, index_res])
-
-
-  predictors = Matrix(transpose(  convert.(Float64, feature_matrix[index_annotation,2])))
-  predictors = reshape(predictors, length(predictors), 1)
-
- # predictors = reduce(vcat, predictors)))
- # hall_of_fame = SymbolicRegression.equation_search(predictors,output)
-  hall_of_fame = RunSR(predictors,output,niterations, options)
-  dominating = calculateParetoFrontier(predictors,output,hall_of_fame,options)
-
-
-
-  trees = [member.tree for member in dominating]
-
-
-
-  res_output = ["Complexity","MSE","Equation"]
-
-  predictions = Any
-
-  for t in trees
-
-
-    output2, did_succeed = eval_tree_array(t, predictors, options)
-    if t == trees[1]
-      predictions =  output2
-    else
-
-      predictions =  hcat(predictions,output2)
-
-    end
-
-
-  end
-
-
-
-  for member in dominating
-      complexity = compute_complexity(member, options)
-      loss = member.loss
-      string = string_tree(member.tree, options)
-
-      res_output = hcat(res_output,[complexity,loss,string])
-  end
-
-
-
-
-  return trees,res_output,predictions,index_annotation
-
 end
 
 
