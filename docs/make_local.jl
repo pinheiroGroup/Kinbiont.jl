e27c1134162087358514ae78dc78b8a2f5c10d5a
Edgar Zanella Alvarenga <e@vaz.io>
2025-03-25 19:02:13 +0100

    update github action for building documention

--- docs/make_local.jl
+++ docs/make_local.jl
@@ -1,12 +1,14 @@
 using Documenter, Kinbiont
+
 push!(LOAD_PATH,"../src/")
-Documenter.Remotes.URL("../src/", nothing)
+#Documenter.Remotes.URL("../src/", nothing)
+
 makedocs(sitename="Kinbiont Documentation",
         modules = [Kinbiont],
         source  = "src/",
         checkdocs=:none,
-        remotes = nothing,
         format = Documenter.HTML(
-        assets = ["assets/favicon.ico"]
+          size_threshold = nothing,
+          assets = ["assets/favicon.ico"]
         )
 )
