98467e5319b9d201442c9fa60efcc05424dc67b7
Edgar Zanella Alvarenga <e@vaz.io>
2026-03-04 10:53:35 +0100

    Wire unified API into module and fix broken Kinbiont.models_list references in ODE/NL model dicts

--- src/Kinbiont.jl
+++ src/Kinbiont.jl
@@ -1,3 +1,9 @@
 module Kinbiont
   include("functions.jl")
+
+  # Unified matrix API
+  include("api/types.jl")
+  include("api/model_registry.jl")
+  include("api/preprocessing.jl")
+  include("api/fitting.jl")
 end
